d44defeadaca2606e822765090cab45d3ac34906
najzermaria <marianajzer@gmail.com>
2023-03-28 18:29:21 +0200

    28-03

--- skrypty/connect.php
+++ skrypty/connect.php
@@ -2,7 +2,7 @@
    $conn = new mysqli("localhost", "root", "", "wsb");
    //echo"db"; 
    //echo $conn->connect_errno;
-   //$conn1 = new mysqli("localhost", "root", "", "wsb");
+   $conn = new mysqli("localhost", "root", "", "wsb", "test");
 
 
 // $sql = "SELECT * FROM `users`;";

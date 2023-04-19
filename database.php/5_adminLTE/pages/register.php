14e270ffd3f3ac2130247c5bdcd7faf6ac18a1fb
najzermaria <marianajzer@gmail.com>
2023-04-19 13:51:49 +0200

    19.04

--- database.php/5_adminLTE/pages/register.php
+++ database.php/5_adminLTE/pages/register.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +18,32 @@
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition register-page">
+
 <div class="register-box">
+<?php
+if(isset($_SESSION["error"])){
+  echo <<< ERROR
+
+  <div class="alert alert-danger alert-dismissible">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                  <h5><i class="icon fas fa-ban"></i> Error!</h5>
+       $_SESSION[error]
+    </div>
+ERROR;
+unset($_SESSION["error"]);
+}
+if(isset($_SESSION["success"])){
+  echo <<< ERROR
+
+  <div class="alert alert-success alert-dismissible">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                  <h5><i class="icon fas fa-ban"></i> Komunikat</h5>
+       $_SESSION[success]
+    </div>
+ERROR;
+unset($_SESSION["success"]);
+}
+?>
   <div class="card card-outline card-primary">
     <div class="card-header text-center">
       <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
@@ -94,7 +122,20 @@
 
         
         <div class="input-group mb-3">
-          <input type="number" class="form-control" placeholder="Podaj id miasta" name="city_id">
+        <select class="custom-select" name="city_id">
+          <?php
+          require_once "../skrypty/connect.php";
+          $sql = "SELECT * from cities";
+          $result = $conn->query($sql);
+          while($city = $result->fetch_assoc()){
+            echo  "<option value='$city[id]'>$city[city]</option>";
+          }
+
+
+          ?>
+          
+            </select>
+
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-city"></span>
@@ -147,3 +188,12 @@
 <script src="../dist/js/adminlte.min.js"></script>
 </body>
 </html>
+
+
+<!-- 
+1) regulamin nie zaznaczony
+2) adresy email są różne
+3) hasła są różne
+
+dodać zmienną pomocniczą error(0), jak będzie błąd to 1
+jeśli error !=0 to cofamy użytkownika i wyświetlamy komunikat(zmienna sesyjna) o błędzie nad formularzem -->
--- database.php/5_adminLTE/skrypty/register.php
+++ database.php/5_adminLTE/skrypty/register.php
@@ -3,16 +3,77 @@
 // print_r($_POST);
 // echo"</pre>";
 
+session_start();
+
+
+	foreach($_POST as $key => $value){
+	
+		if (empty($value)){
+			$_SESSION["error"] = "Wypełnij wszystkie pola!";
+			echo $_SESSION["error"];
+			echo "<script>history.back();</script>";
+			exit();
+		}
+	}
+
+	$error = 0;
+	
+	//regulamin
+	if (!isset($_POST["terms"])){
+		$error = 1;
+		$_SESSION["error"] = "Zatwierdź regulamin!";
+	}
+	// hasło
+	if($_POST["pass1"] != $_POST["pass2"]){
+		$error =1;
+		$_SESSION["error"] = "Hasła są różne";
+	}
+	// mail
+	if($_POST["email1"] != $_POST["email2"]){
+		$error =1;
+		$_SESSION["error"] = "Adresy email są różne";
+	}
+	
+		
+	if($error != 0){
+			echo "<script>history.back();</script>";
+		exit();
+		}
+	
+// duplikacja maila!!!!!!
+
+
 
 require_once "./connect.php";
 	$stmt = $conn->prepare("INSERT INTO users (`email`, `city_id`, `firstName`, `lastName`, `birthday`, `password`, `created_at`) VALUES (?, ?, ?, ?, ?, ?, current_timestamp());");
 
-	$stmt->bind_param('sissss', $_POST["email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["pass1"]);
+	$pass = password_hash('$_POST["pass1"]', algo: PASSWORD_ARGON2ID);
+
+	$stmt->bind_param('sissss', $_POST["email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $pass);
 
     $stmt->execute();
 
 	echo $stmt->affected_rows;
 
+	
+
 	//echo password_hash("rasmuslerdorf", PASSWORD_DEFAULT);
 
+	if($stmt->affected_rows == 1){
+		$_SESSION["success"] = "Dodano użytkownika! $_POST[firstName] $_POST[lastName]";
+		
+	}else{
+		$_SESSION["error"] = "Nie udało się dodać rekordu!";
+	}
+
+	header("location: ../pages/register.php");
+
 ?>
+
+
+<!-- 1) regulamin nie zaznaczony
+2) adresy email są różne
+3) hasła są różne
+
+dodać zmienną pomocniczą error(0), jak będzie błąd to 1
+jeśli error !=0 to cofamy użytkownika i wyświetlamy komunikat(zmienna sesyjna) o błędzie nad formularzem -->
